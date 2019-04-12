0183fc92985ca08e4ba95e2e3ff11e7377d518d0
Sally Haefling <sallyhaefling@gmail.com>
2019-04-11 20:55:38 -0600

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Card from '../Card/Card.js';
+import './Styles/index.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        Hello world!
+      </section>
+    );
+  }
+}
+
+export default App;
