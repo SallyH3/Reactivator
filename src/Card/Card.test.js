32bc037bc07b0fce117174acd66c48cecc036500
Sally Haefling <sallyhaefling@gmail.com>
2019-04-14 13:24:15 -0600

    SetupTests file and add state test to card

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -1,3 +1,21 @@
 import React from 'react';
-import Search from './Card';
+import Card from './Card';
 import { shallow } from 'enzyme';
+
+describe('Card', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <Card />
+    )
+  });
+
+  it('should have default states', () => {
+    expect(wrapper.state()).toEqual({
+      input: '',
+      message: '',
+      incorrectCards: []
+    })
+  });
+
+});
