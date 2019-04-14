5eab904e8b034359a3d4ef4cf5339aa5b3e0cfff
Sally Haefling <sallyhaefling@gmail.com>
2019-04-13 19:10:23 -0600

    Local storage and hide card

--- src/App/App.js
+++ src/App/App.js
@@ -3,16 +3,71 @@ import Card from '../Card/Card.js';
 import Controls from '../Controls/Controls.js';
 import '../Styles/index.scss';
 import data from '../data.js';
-import Popup from '../Popup/Popup.js';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
       currentCard: null,
-      cards: [],
-      currentCards: []
+      quiz: [],
+      incorrectCards: []
     }
+  }
+
+  getStorage = () => {
+    const newQuiz = data.map(x => {
+      return {...x, answered: false
+      }
+    })
+    const incorrectAnswers = localStorage.getItem('incorrectAnswers')
+    if(incorrectAnswers !== null) {
+      this.setState({
+        quiz: newQuiz,
+        incorrectCards: JSON.parse(incorrectAnswers)
+      })
+    }
+    this.setState({
+      quiz: newQuiz
+    })
+  }
+
+  setStorage = (id) => {
+    const incorrectCard = data.find(card => {
+      return card.id === id;
+    })
+    const checkRepeatCard = this.state.incorrectCards.find(card => {
+      return card.id === id;
+    })
+    if(!checkRepeatCard) {
+      const updateIncorrectCards = [...this.state.incorrectCards, incorrectCard]
+      this.setState({
+        incorrectCards: updateIncorrectCards
+      }, 
+      () => {
+        localStorage.setItem('incorrectAnswers', JSON.stringify(updateIncorrectCards))
+      }
+      )
+    }
+  }
+
+  componentDidMount = () => {
+    this.getStorage();
+  }
+
+  getCards = (id) => {
+    const card = this.state.quiz.map(x => {
+      if(id === x.id) {
+        //makes new object that includes answered: true now
+        //making the answered true after finding id
+        return {...x, answered: true
+        }
+      } else {
+        return x;
+      }
+    })
+    this.setState({
+      quiz: card
+    })
   }
 
   checkReturnCard = () => {
@@ -38,22 +93,21 @@ export default class App extends Component {
           a react.js study tool
         </p>
       {
-        data.map(trivia => 
-          <Card 
+        this.state.quiz.map(trivia => {
+          if(!trivia.answered) {
+          return (<Card 
+          getCards = {this.getCards}
+          id={trivia.id}
           key={trivia.id}
           subject={trivia.subject}
           question={trivia.question}
           answer={trivia.answer}
+          setStorage={this.setStorage}
           />)
-      }
-      {
-      data.map(trivia => 
-        <Popup
-          key={trivia.id}
-          subject={trivia.subject}
-          question={trivia.question}
-          answer={trivia.answer}
-          />)
+          } else {
+            return null
+          }
+        })
       }
       {this.checkReturnCard()}
       </section>
--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Popup from '../Popup/Popup.js';
 
 export default class Controls extends Component {
   constructor(props) {
@@ -13,25 +12,19 @@ export default class Controls extends Component {
   }
 
   displayIncorrectCards = () => {
-    // to display incorrectly answered cards - will happen on click of the button, local storage is keeping track of incorrect cards inside of Cards.js component
+    // to display incorrectly answered cards - will happen on click of the button, local storage is keeping track of incorrect cards inside of App.js component
   }
 
   render() {
     return  (
       <section className='button-container'>
-      {/* on click, scroll bar will display with cards they got wrong - will be hidden until they click */}
           <input 
           value='Study list'
           type='submit'
           className='study-button'
+          {/* on click, will hide current unanswered cards and display incorrect cards, take out 'togglePopup' */}
           onClick={this.togglePopup}
           />
-          {this.state.showPopup ? 
-          <Popup
-            closePopup={this.togglePopup}
-          />
-          : null
-        }
       </section>
     )
   }
