40d04fc0ed641f3d6d7db70c8692986415c39da2
Sally Haefling <sallyhaefling@gmail.com>
2019-04-16 12:58:59 -0600

    Add more tests

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
- 
+
 describe ('App', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <App data />
+      <App />
     )
   });
 
@@ -27,6 +27,42 @@ it('renders without crashing', () => {
       quiz: [],
       incorrectCards: [],
       showReview: false
-    })
-  })
+    });
+  });
+
+  it ('should setState in getCards method', () => {
+wrapper.setState({quiz:[{
+  subject: "JSX",
+  question: "JSX represents objects called what?",
+  answer: "React elements",
+  correct: false,
+  id: 1
+  },
+  {
+  subject: "JSX",
+  question: "JSX is a combination of these two languages",
+  answer: "HTML and JavaScript",
+  correct: false,
+  id: 2
+  }]}) 
+wrapper.instance().getCards(1);
+expect(wrapper.state('quiz')).toEqual([{subject: "JSX",
+question: "JSX represents objects called what?",
+answer: "React elements",
+correct: false,
+answered: true,
+id: 1},
+{
+  subject: "JSX",
+  question: "JSX is a combination of these two languages",
+  answer: "HTML and JavaScript",
+  correct: false,
+  id: 2
+  }
+]);
+  });
+
+  it('should setState in getAllCards method', () => {
+    
+  });
 });
